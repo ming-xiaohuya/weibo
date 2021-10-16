a809f46e7c12c2cd8eb158f5d76551b271a87f70
ming-xiaohuya <2182107058@qq.com>
2021-10-17 03:20:22 +0800

    发布微博

--- app/Models/Status.php
+++ app/Models/Status.php
@@ -9,6 +9,7 @@ class Status extends Model
 {
     use HasFactory;
 
+    protected $fillable = ['content'];  // 指定在微博模型中可以进行正常更新的字段
     // 关联数据表，多对一
     public function user() {
         return $this->belongsTo(User::class);
